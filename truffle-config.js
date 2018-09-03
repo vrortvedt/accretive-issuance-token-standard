394be16847d05ef2691588a644b7430dab124ea0
vrortvedt <38442505+vrortvedt@users.noreply.github.com>
2018-09-02 20:48:41 -0400

    Update truffle-config.js

--- truffle-config.js
+++ truffle-config.js
@@ -1,15 +1,8 @@
 module.exports = {
-  rinkeby: {
-    host: "localhost", // Connect to geth on the specified
-    port: 8545,
-    from: "0x7f7dc314fc75658d474dbbfe598ebb058ca6aacd", // default address to use for any transaction Truffle makes during migrations
-    network_id: 4,
-    gas: 4612388 
-  },
   networks: {
     development: {
       host: "127.0.0.1",
-      port: 7545,
+      port: 8545,
       network_id: "*"
     }
   }
