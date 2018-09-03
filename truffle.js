86983603765a096793aa3005f11fefe570ba6373
vrortvedt <38442505+vrortvedt@users.noreply.github.com>
2018-09-02 20:48:07 -0400

    Update truffle.js

--- truffle.js
+++ truffle.js
@@ -1,19 +1,11 @@
 
-require('dotenv').config();
-const HDWalletProvider = require("truffle-hdwallet-provider");
-
 module.exports = {
   networks: {
     development: {
       host: "localhost",
-      port: 7545,
+      port: 8545,
       gas: 6500000,
       network_id: "5777"
     },
-    rinkeby: {
-    provider: new HDWalletProvider(process.env.MNEMONIC, "https://rinkeby.infura.io/" + process.env.INFURA_API_KEY),
-    network_id: 4,
-    gas: 4500000
-	},
   }
 };
